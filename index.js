4d2872bcd675ea9297aa58368f7bfb6c1a92144e
Andy Choi <wooseokchoi96@gmail.com>
2019-09-15 16:21:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let cards = [];
+    for(let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+function countdown(number) {
+    for(let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
